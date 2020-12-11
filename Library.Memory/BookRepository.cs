18040acb0cc0006df3362fa91f10ebe64df6cdb6
Олексій Бондарчук <alexalex60421@gmail.com>
2020-12-11 15:30:33 +0200

    infrastructure add

---
+++ Library.Memory/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Library.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books =
+        {
+            new Book(1,"Book #1 foz"),
+            new Book(2,"foz Book #2"),
+            new Book(3,"Book #3")
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
